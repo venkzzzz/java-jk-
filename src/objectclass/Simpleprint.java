fefcc70b38238c39c87ff57a5f9b65c4a9ce4e85
Baskar <Baskar@DESKTOP-OCAV2ED>
2024-01-29 17:33:26 +0530

    initial commit

---
+++ src/objectclass/Simpleprint.java
@@ -0,0 +1,12 @@
+package objectclass;
+
+public class Simpleprint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Hi \ngood");
+      // System.out.println("I'm student from Besant");
+       // System.out.println("I'm doing my Java");
+	}
+
+}
